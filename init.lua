288a375a135c07399cd469eb53758da918d60e11
csecsoliver <0oliogamer0@gmail.com>
2025-10-04 20:07:48 +0200

    asd

--- init.lua
+++ init.lua
@@ -255,7 +255,8 @@ require('lazy').setup({
   --
   -- Use `opts = {}` to automatically pass options to a plugin's `setup()` function, forcing the plugin to be loaded.
   --
-
+  
+  { 'wakatime/vim-wakatime', lazy = false },
   -- Alternatively, use `config = function() ... end` for full control over the configuration.
   -- If you prefer to call `setup` explicitly, use:
   --    {
